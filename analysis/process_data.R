71264181778b130cd9c0d64269d0ad8aa353d322
mzettersten <mzettersten@users.noreply.github.com>
2024-12-04 12:57:08 -0800

    final update to data

--- analysis/process_data.R
+++ analysis/process_data.R
@@ -35,6 +35,31 @@ processed_data <- exp_data %>%
   select(-response,-type) %>%
   rename(likert_response=Q0) %>%
   relocate(likert_response,.after=word)
+
+#filter participant ids
+filter_ids <- c(
+  "1","12","1234","a1","as","nm","p4","p6","shrew123","tardigrade666"
+)
+
+processed_data <- processed_data %>%
+  mutate(participant_id = trimws(tolower(participant_id))) %>%
+  #fix some ids
+  mutate(
+    participant_id = case_when(
+      participant_id == "herson" ~ "heron",
+      participant_id == "p73" ~ "giraffe",
+      participant_id == "2341" ~ "porcupine",
+      participant_id == "a16486413" ~ "beaver",
+      participant_id == "a16995566" ~ "squirrel",
+      participant_id == "a17287899" ~ "trex",
+      participant_id == "a17689315" ~ "rabbit",
+      participant_id == "a1789315" ~ "rabbit",
+      participant_id == "a17922543" ~ "llama",
+      participant_id == "a18151131" ~ "rhino",
+      TRUE ~ participant_id
+    )
+  ) %>%
+  filter(!(participant_id %in% filter_ids))
   
 #store processed and prepped data
 write_csv(processed_data,here(processed_data_directory,paste0(file_name,"-processed-data.csv")))
